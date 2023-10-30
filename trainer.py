eff9f8e14a223e7bf35c09fc2d0591c730109a8e
Cwoasount <78689800+eKondrup@users.noreply.github.com>
2023-10-30 22:21:45 +0100

    Muliggjorde ny struktur af data således data kommer kan komme i denne form (n_features*n_channels)*n_segments

--- trainer.py
+++ trainer.py
@@ -21,6 +21,8 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+encoder = OneHotEncoder(sparse_output=False)
 
 #Sti til data og omdannelse til pandas Dataframe
 trail = "1"
@@ -85,76 +87,115 @@ n_segments = 33
 n_channels = 8
 
 
-def load_features(train_files, test_files, is_channel_feature=True, verbose=True):
+def load_features(files, one_hot_encoded_label, is_channel_feature=True, verbose=True):
     ################
     #Læsning af features. 
     #Features bliver gemt i feature_list, som har en shape på 8*33*4
     ################
     files_feature_list_2d = np.empty((0,5))
-    
-    for file in train_files:
-        #Denne variabel bliver kun brugt til at determinere længden af en celle i loopet herunder
-        df_window_list_len = len(file.iloc[:,0])
-        feature_list = []
-    
-        for j in range(len(file.columns)): #Looper over kolonnerne i read
-    
-            col_feature = []
-        
-            for i in range(df_window_list_len): #Looper over rækkerne
-        
-        
-                # Brug ast.literal_eval til at konvertere stringen tilbage til en liste
-                feature = ast.literal_eval(file.iloc[i, j])
-                               
-                col_feature.append(feature)     
-        
-            feature_list.append(col_feature)
-        
-        feature_list_3D = np.array(feature_list)
-       
-            
-        #Dette giver en liste på denne form
-        #[feat1,feat2,feat3,feat4]
-        #Med 264 (8*33) rækker
-        feature_list_2d = feature_list_3D.reshape(-1, n_features)
-        # print(feature_list_2d)
-        # Laver en liste med hver channel i [0,1..,7]
-        #Der bliver brugt en list comprehension: https://stackoverflow.com/questions/6475314/python-for-in-loop-preceded-by-a-variable
-        channels_list = np.array([l for l in range(n_channels)]).flatten()
-
-        #Listen bliver gentaget n_segmetns n_segments gange og samme elementer
-        #Sættes ved siden af hinanden [0*33,1*33...] ish..
-        channels_list = np.repeat(channels_list, n_segments)
-        
-        #TODO: Find ud af hvad dette betyder
-        channels_list_reshaped = channels_list[:feature_list_2d.shape[0]].reshape(-1, 1)
-        
-        #Channel featuren blev tilføjet til en liste
-        feature_list_2d_with_channel = np.column_stack((channels_list_reshaped, feature_list_2d))
-        # print(feature_list_2d_with_channel)
-
-        #feature_list_2d_with_channel bliver tilføjet til et nyt np array der holder alle features fra alle filerne
-        files_feature_list_2d = np.append(files_feature_list_2d, feature_list_2d_with_channel, axis=0)
-
-    if verbose==True:
-        
-        #Dette giver Pandas lov til at dataframes må fylde hele deres dimensioner i konsollen
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-
-        print(f"Dataframe af features*segments med channels som feature\n{pd.DataFrame(feature_list_2d_with_channel)}")
-        print(f"Dataframe af samlet features*segments med channels som feature\n{pd.DataFrame(files_feature_list_2d)}")
-    #Resetter 
-        
-        
     if is_channel_feature==True:
-        return feature_list_2d_with_channel    
-       
-    if is_channel_feature == False:
-        feature_list_3D = np.transpose(feature_list_3D, (1, 0, 2))
-        return feature_list_3D
+        for file in files:
+            #Denne variabel bliver kun brugt til at determinere længden af en celle i loopet herunder
+            df_window_list_len = len(file.iloc[:,0])
+            feature_list = []
         
+            for j in range(len(file.columns)): #Looper over kolonnerne i read
+        
+                col_feature = []
+            
+                for i in range(df_window_list_len): #Looper over rækkerne
+            
+            
+                    # Brug ast.literal_eval til at konvertere stringen tilbage til en liste
+                    feature = ast.literal_eval(file.iloc[i, j])
+                                
+                    col_feature.append(feature)     
+            
+                feature_list.append(col_feature)
+            
+            feature_list_3D = np.array(feature_list)
+            # print(feature_list_3D)
+                
+            #Dette giver en liste på denne form
+            #[feat1,feat2,feat3,feat4]
+            #Med 264 (8*33) rækker
+            feature_list_2d = feature_list_3D.reshape(-1, n_features)
+            # print(feature_list_2d)
+            # Laver en liste med hver channel i [0,1..,7]
+            #Der bliver brugt en list comprehension: https://stackoverflow.com/questions/6475314/python-for-in-loop-preceded-by-a-variable
+            channels_list = np.array([l for l in range(n_channels)]).flatten()
+
+            #Listen bliver gentaget n_segmetns n_segments gange og samme elementer
+            #Sættes ved siden af hinanden [0*33,1*33...] ish..
+            channels_list = np.repeat(channels_list, n_segments)
+            
+            #TODO: Find ud af hvad dette betyder
+            channels_list_reshaped = channels_list[:feature_list_2d.shape[0]].reshape(-1, 1)
+            
+            #Channel featuren blev tilføjet til en liste
+            feature_list_2d_with_channel = np.column_stack((channels_list_reshaped, feature_list_2d))
+            # print(feature_list_2d_with_channel)
+
+            #feature_list_2d_with_channel bliver tilføjet til et nyt np array der holder alle features fra alle filerne
+            files_feature_list_2d = np.append(files_feature_list_2d, feature_list_2d_with_channel, axis=0)
+
+        if verbose==True:
+            
+            #Dette giver Pandas lov til at dataframes må fylde hele deres dimensioner i konsollen
+            pd.set_option('display.max_rows', None)
+            pd.set_option('display.max_columns', None)
+
+            print(f"Dataframe af features*segments med channels som feature\n{pd.DataFrame(feature_list_2d_with_channel)}")
+            print(f"Dataframe af samlet features*segments med channels som feature\n{pd.DataFrame(files_feature_list_2d)}")
+        #Resetter 
+        return feature_list_2d_with_channel    
+    
+
+    if is_channel_feature == False: 
+        #Hvis channel ikke er en feature skal dataen opstilles i dette format: (n_features*channels)*segments altså
+        #den første række bliver features for hver channel dvs. der bliver n_features*n_channels = 32 kolonner
+
+        #Denne variabel initialiseres på denne måde da den ellers ikke kan blive appendet til, fordi den ikke har korrekte mængde af kolonner
+        #np.empty((0,32)) laver et tomt np array med 0 rækker og 32 kolonner
+        files_feature_list = np.empty((0,32))
+        files_feature_list_labels = []
+
+        for file_nr,file in enumerate(files): #Looper over filerne
+
+            for feature_list_str in file.values: #Looper over rækkerne
+
+                feature_list_row = []
+
+                for feature_list in feature_list_str: #Looper over elementer i csv filen altså "[feat1,...],[feat1...]...[]"
+
+                    #Elementet konverteres fra en string tilbage til en liste. Feature_list holder derfor 4 elementer [f1,f2,f3,f4]
+                    feature_list = ast.literal_eval(feature_list)
+                    
+                    #feature_list tilføjes til feature_list_row. Dvs feature_list_row får 32 elementer
+                    feature_list_row = np.append(feature_list_row, feature_list)
+
+                #feature_list_row tilføjes til et nyt array på den vertikale akse
+                files_feature_list = np.vstack((files_feature_list, feature_list_row))
+
+                files_feature_list_labels = np.append(files_feature_list_labels, file_nr)
+            
+        if verbose==True:
+            print(files_feature_list.shape)
+            # print(pd.DataFrame(files_feature_list))
+            print(files_feature_list_labels)
+
+        if one_hot_encoded_label == True:
+
+            files_feature_list_labels = np.array(files_feature_list_labels).reshape(-1, 1)
+            onehot_files_feature_list_labels = encoder.fit_transform(files_feature_list_labels)
+            
+            return files_feature_list, onehot_files_feature_list_labels
+        else:
+            return files_feature_list, files_feature_list_labels
+
+        
+    if is_channel_feature == "3D" or is_channel_feature == "3d":
+        return feature_list_3D
     else: 
             print("Forkert parameter var givet")
     
@@ -163,17 +204,23 @@ def load_features(train_files, test_files, is_channel_feature=True, verbose=True
 pd.reset_option('display.max_rows')
 pd.reset_option('display.max_columns')
 
-def prepare_data(train_files, test_files, is_channel_feature=True, verbose=True):
-    
-    
-        
-    return load_features(train_files, test_files, is_channel_feature, verbose), 
-    
-    
-        
-prepare_data(train_files, None, verbose=False)
-    
-'''
-x_train = load_features()
+def prepare_data(train_files, test_files, one_hot_encoded_label, is_channel_feature=True, verbose=True):
+            
+    x_train, y_train = load_features(train_files, one_hot_encoded_label, is_channel_feature, verbose)
 
-y_train = load_labels()'''
+    x_test, y_test = load_features(test_files, one_hot_encoded_label, is_channel_feature, verbose)
+
+    return x_train,y_train,x_test,y_test
+
+
+    
+lda = LinearDiscriminantAnalysis()
+x_train, y_train, x_test, y_test = prepare_data(train_files, test_files, one_hot_encoded_label=False, is_channel_feature=False, verbose=False)
+
+lda.fit(x_train, y_train)
+
+score = lda.score(x_test, y_test)
+print(f'Accuracy: {score}')
+
+y_pred = lda.predict(x_test)
+# print(y_pred)
